accf37d47b0eae4f1a75c3f84cdc1fd023823d58
Luis Cruz <luis.cruz@ist.utl.pt>
2009-01-23 18:12:20 +0000

    Moved module specific stuff to respective module.

--- bennu-core/src/myorg/domain/contents/ActionNode.java
+++ bennu-core/src/myorg/domain/contents/ActionNode.java
@@ -1,12 +1,8 @@
 package myorg.domain.contents;
 
-import java.math.BigDecimal;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
-import module.coffee.domain.CoffeeItem;
-import module.coffee.domain.ItemBean;
-import myorg.domain.MyOrg;
 import myorg.domain.groups.PersistentGroup;
 import myorg.presentationTier.Context;
 import pt.ist.fenixWebFramework.services.Service;
@@ -28,41 +24,7 @@ public class ActionNode extends ActionNode_Base {
 	actionNode.setLinkBundle(bundle);
 	actionNode.setLinkKey(key);
 	actionNode.setAccessibilityGroup(accessibilityGroup);
-
-	initializeItems();
-
 	return actionNode;
-    }
-
-    private static void initializeItems() {
-	if (!MyOrg.getInstance().hasAnyCoffeeItems()) {
-	    createItem("Nespresso Variations Crystalized Ginger", "nesclub.nespresso.com/img/pictures/02377.jpg", "0.36", Boolean.TRUE);
-	    createItem("Nespresso Variations Caramel", "nesclub.nespresso.com/img/pictures/02372.jpg", "0.36", Boolean.TRUE);
-	    createItem("Nespresso Vartiations Mandarine", "nesclub.nespresso.com/img/pictures/02376.jpg", "0.36", Boolean.TRUE);
-
-	    createItem("Ristretto", "nesclub.nespresso.com/img/pictures/00746.gif", "0.31", Boolean.TRUE);
-	    createItem("Arpeggio", "nesclub.nespresso.com/img/pictures/00673.gif", "0.31", Boolean.TRUE);
-	    createItem("Roma", "nesclub.nespresso.com/img/pictures/00747.gif", "0.31", Boolean.TRUE);
-	    createItem("Decaffeinato Intenso", "nesclub.nespresso.com/img/pictures/00707.gif", "0.31", Boolean.TRUE);
-	    createItem("Livanto", "nesclub.nespresso.com/img/pictures/00712.gif", "0.31", Boolean.TRUE);
-	    createItem("Capriccio", "nesclub.nespresso.com/img/pictures/00674.gif", "0.31", Boolean.TRUE);
-	    createItem("Volluto", "nesclub.nespresso.com/img/pictures/00751.gif", "0.31", Boolean.TRUE);
-	    createItem("Cosi", "nesclub.nespresso.com/img/pictures/00676.gif", "0.31", Boolean.TRUE);
-	    createItem("Decaffeinato", "nesclub.nespresso.com/img/pictures/00706.gif", "0.31", Boolean.TRUE);
-
-	    createItem("Vivalto", "nesclub.nespresso.com/img/pictures/00750.gif", "0.33", Boolean.TRUE);
-	    createItem("Decaffeinato Lungo", "nesclub.nespresso.com/img/pictures/00708.gif", "0.33", Boolean.TRUE);
-	    createItem("Finezzo", "nesclub.nespresso.com/img/pictures/00709.gif", "0.33", Boolean.TRUE);
-	}
-    }
-
-    private static void createItem(final String name, final String imageUrl, final String unitValue, final Boolean available) {
-	final ItemBean itemBean = new ItemBean();
-	itemBean.setName(name);
-	itemBean.setUnitValue(new BigDecimal(unitValue));
-	itemBean.setImageUrl(imageUrl);
-	itemBean.setAvailable(available);
-	CoffeeItem.creanteNewItem(itemBean);
     }
 
     @Override
