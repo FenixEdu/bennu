36ccc9a332b645e0647ce30e0a3a00e9aa8427b7
Luis Cruz <luis.cruz@ist.utl.pt>
2009-01-27 12:25:01 +0000

    Improve error handleing of retieving resources from resource bundles.

--- bennu-core/src/myorg/domain/contents/ActionNode.java
+++ bennu-core/src/myorg/domain/contents/ActionNode.java
@@ -1,12 +1,9 @@
 package myorg.domain.contents;
 
-import java.util.Locale;
-import java.util.ResourceBundle;
-
 import myorg.domain.groups.PersistentGroup;
 import myorg.presentationTier.Context;
+import myorg.util.BundleUtil;
 import pt.ist.fenixWebFramework.services.Service;
-import pt.utl.ist.fenix.tools.util.i18n.Language;
 import pt.utl.ist.fenix.tools.util.i18n.MultiLanguageString;
 
 public class ActionNode extends ActionNode_Base {
@@ -34,31 +31,9 @@ public class ActionNode extends ActionNode_Base {
 
     @Override
     public MultiLanguageString getLink() {
-	final MultiLanguageString multiLanguageString = new MultiLanguageString();
 	final String bundle = getLinkBundle();
 	final String key = getLinkKey();
-	if (!addContent(multiLanguageString, bundle, Language.getLocale(), key)) {
-	    if (!addContent(multiLanguageString, bundle, Language.getDefaultLocale(), key)) {
-		addContent(multiLanguageString, bundle, null, key);
-	    }
-	}
-	return multiLanguageString;
-    }
-
-    private boolean addContent(final MultiLanguageString multiLanguageString, final String bundle, final Locale locale, final String key) {
-	final ResourceBundle resourceBundle;
-	if (locale == null) {
-	    resourceBundle = ResourceBundle.getBundle(getLinkBundle());
-	} else {
-	    resourceBundle = ResourceBundle.getBundle(getLinkBundle(), Language.getLocale());
-	}
-	if (resourceBundle != null) {
-	    final Language language = Language.valueOf(locale.getLanguage());
-	    final String content = resourceBundle.getString(key);
-	    multiLanguageString.setContent(language, content);
-	    return true;
-	}
-	return false;
+	return BundleUtil.getMultilanguageString(bundle, key);
     }
 
     @Override
--- bennu-core/src/myorg/util/BundleUtil.java
+++ bennu-core/src/myorg/util/BundleUtil.java
@@ -1,14 +1,30 @@
 package myorg.util;
 
+import java.util.Locale;
 import java.util.ResourceBundle;
 
 import pt.utl.ist.fenix.tools.util.i18n.Language;
+import pt.utl.ist.fenix.tools.util.i18n.MultiLanguageString;
 
 public class BundleUtil {
 
     public static String getStringFromResourceBundle(final String bundle, final String key) {
 	final ResourceBundle resourceBundle = ResourceBundle.getBundle(bundle, Language.getLocale());
 	return resourceBundle.getString(key);	
+    }
+
+    public static MultiLanguageString getMultilanguageString(final String bundle, final String key) {
+	final MultiLanguageString multiLanguageString = new MultiLanguageString();
+	final Locale locale = Language.getLocale();
+	final ResourceBundle resourceBundle = ResourceBundle.getBundle(bundle, locale);
+	final Language language = Language.valueOf(locale.getLanguage());
+	if (resourceBundle != null && resourceBundle.containsKey(key)) {
+	    final String content = resourceBundle.getString(key);
+	    multiLanguageString.setContent(language, content);
+	} else {
+	    multiLanguageString.setContent(language, "???" + key + "???");
+	}
+	return multiLanguageString;
     }
 
 }
