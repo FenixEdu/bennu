ec614d83d66f0ac327fab47e432ff4f073341a97
Luis Cruz <luis.cruz@ist.utl.pt>
2009-02-17 18:20:03 +0000

    Created mechanism to jump into first accessible child node.

--- bennu-core/src/myorg/presentationTier/actions/HomeAction.java
+++ bennu-core/src/myorg/presentationTier/actions/HomeAction.java
@@ -111,6 +111,8 @@ public class HomeAction extends ContextBaseAction {
 	final SortedSet<Node> nodes = virtualHost.getOrderedTopLevelNodes();
 	for (final Node node : nodes) {
 	    if (node.isAccessible()) {
+		final Context context = getContext(request);
+		context.push(node);
 		return new ActionForward(node.getUrl());
 	    }
 	}
