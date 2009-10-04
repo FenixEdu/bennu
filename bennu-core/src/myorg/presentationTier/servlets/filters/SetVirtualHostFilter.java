ec92ae43e5a5ca3fd3256a3cdd9af39a9acd02b1
Luis Cruz <luis.cruz@ist.utl.pt>
2009-10-04 22:29:08 +0000

    Removed debug code.

--- bennu-core/src/myorg/presentationTier/servlets/filters/SetVirtualHostFilter.java
+++ bennu-core/src/myorg/presentationTier/servlets/filters/SetVirtualHostFilter.java
@@ -40,29 +40,23 @@ public class SetVirtualHostFilter implements Filter {
 
     @Override
     public void init(final FilterConfig filterConfig) throws ServletException {
-	System.out.println("Initializing set virtual host filter.");
     }
 
     @Override
     public void destroy() {
-	System.out.println("Destroying set virtual host filter.");
     }
 
     @Override
     public void doFilter(final ServletRequest servletRequest, final ServletResponse servletResponse, final FilterChain filterChain)
     		throws IOException, ServletException {
-	System.out.println("Entering set virtual host doFilter.");
 	final String serverName = servletRequest.getServerName();
-	System.out.println("serverName: " + serverName);
 	try {
 	    final VirtualHost virtualHost = VirtualHost.setVirtualHostForThread(serverName.toLowerCase());
-	    System.out.println("Found virtual host: " + virtualHost.getHostname());
 	    servletRequest.setAttribute("virtualHost", virtualHost);
 	    filterChain.doFilter(servletRequest, servletResponse);
 	} finally {
 	    VirtualHost.releaseVirtualHostFromThread();
 	}
-	System.out.println("Exiting set virtual host doFilter.");
     }
 
 }
