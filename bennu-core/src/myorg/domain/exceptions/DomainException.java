cbab43c344129786cc7d2de124f0cf084d31ff8f
Paulo Abrantes <paulo.abrantes@ist.utl.pt>
2009-12-16 17:01:42 +0000

    Support for localization within DomainException

--- bennu-core/src/myorg/domain/exceptions/DomainException.java
+++ bennu-core/src/myorg/domain/exceptions/DomainException.java
@@ -25,35 +25,92 @@
 
 package myorg.domain.exceptions;
 
+import java.util.ResourceBundle;
+
+import pt.utl.ist.fenix.tools.util.i18n.Language;
+
 public class DomainException extends Error {
 
-	private final String key;
+    private final String key;
 
-	private final String[] args;
+    private final String[] args;
 
-	public DomainException() {
-		this(null, (String[]) null);
+    private final String bundle;
+
+    public DomainException() {
+	this(null, (String[]) null);
+    }
+
+    public DomainException(final String key, final String... args) {
+	super(key);
+	this.key = key;
+	this.args = args;
+	this.bundle = null;
+    }
+
+    public DomainException(final String key, final Throwable cause, final String... args) {
+	super(key, cause);
+	this.key = key;
+	this.args = args;
+	this.bundle = null;
+    }
+
+    public DomainException(final String key, final String bundle) {
+	super(key);
+	this.key = key;
+	this.bundle = bundle;
+	this.args = null;
+    }
+
+    public DomainException(final String key, final String bundle, final String... args) {
+	super(key);
+	this.key = key;
+	this.bundle = bundle;
+	this.args = args;
+    }
+
+    public String getKey() {
+	return key;
+    }
+
+    public String[] getArgs() {
+	return args;
+    }
+
+    public String getBundle() {
+	return bundle;
+    }
+
+    @Override
+    public String getLocalizedMessage() {
+	if (getBundle() == null) {
+	    return getMessage();
 	}
+	return (getArgs() != null) ? getFormattedStringFromResourceBundle(getBundle(), getKey(), getArgs())
+		: getStringFromResourceBundle(getBundle(), getKey());
+    }
 
-	public DomainException(final String key, final String... args) {
-		super(key);
-		this.key = key;
-		this.args = args;
-	}
+    /*
+     * This actually has been copied from myorg.util.BundleUtil because we don't
+     * want to add such deppency to DomainException. Although this code should
+     * be refactored to a generic util, so it would be delegated instead of
+     * being repeated.
+     * 
+     * PS: This code is repeated because bundleUtil deppends of
+     * MultiLanguateString for example. And both me and LC felt that it made no
+     * sence.
+     */
+    private String getStringFromResourceBundle(final String bundle, final String key) {
+	final ResourceBundle resourceBundle = ResourceBundle.getBundle(bundle, Language.getLocale());
+	return resourceBundle.getString(key);
+    }
 
-	public DomainException(final String key, final Throwable cause,
-			final String... args) {
-		super(key, cause);
-		this.key = key;
-		this.args = args;
+    private String getFormattedStringFromResourceBundle(final String bundle, final String key, String... arguments) {
+	String resourceString = getStringFromResourceBundle(bundle, key);
+	for (int i = 0; i < arguments.length; i++) {
+	    resourceString = resourceString.replace("{" + i + "}", arguments[i]);
 	}
-
-	public String getKey() {
-		return key;
-	}
-
-	public String[] getArgs() {
-		return args;
-	}
+	return resourceString;
+    }
 
 }
