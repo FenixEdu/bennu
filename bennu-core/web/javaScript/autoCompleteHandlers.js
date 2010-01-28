c951ee0c90d42ae48f5b69e980e77d7c9ac9b4e1
Paulo Abrantes <paulo.abrantes@ist.utl.pt>
2010-01-28 00:51:25 +0000

    Bug fix regarding loosing the value in the text field of a auto complete renderer (forgot this file ... :S )

--- bennu-core/web/javaScript/autoCompleteHandlers.js
+++ bennu-core/web/javaScript/autoCompleteHandlers.js
@@ -1,15 +1,17 @@
 function clearAutoComplete(input,inputText) {
-	id = escapeId(input.attr('id')) + "_AutoComplete";
+	var id = escapeId(input.attr('id')) + "_AutoComplete";
 	$("input#" + id).val("");
 	$(input).parents("td").next("td:last").fadeOut();
 }
 function updateCustomValue(input,inputText) {
-	id = escapeId(input.attr('id')) + "_OldValue";
+	var id = escapeId(input.attr('id')) + "_OldValue";
 	$("input#" + id).val( inputText);
 	$("input#" + escapeId(input.attr('id')) + "_AutoComplete").val("custom");
+	var name = escapeId(input.attr('id'));
+	$("input[name='" +  name + "']").val($("input#" + id).val());
 }
 function selectElement(input,inputText,element) {
-	id = escapeId(input.attr('id'));  
+	var id = escapeId(input.attr('id'));  
 	$("input#" + id + "_AutoComplete").val(element.attr('id')); 
 	$("input#" + id + "_OldValue").val( inputText);
 	$("input#" + id + "_AutoComplete").trigger("change");
