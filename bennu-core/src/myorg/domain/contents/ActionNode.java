6da927b7dc4117c0ff38cb5e3cc594f2a88625e8
Luis Cruz <luis.cruz@ist.utl.pt>
2010-11-08 17:38:11 +0000

    New Feature: Added VaadinNode to make the creation of nodes to vaadin interfaces a bit easier. Also added a helpful struts action that forwards to vaadin interface.

--- bennu-core/src/myorg/domain/contents/ActionNode.java
+++ bennu-core/src/myorg/domain/contents/ActionNode.java
@@ -33,21 +33,30 @@ import pt.utl.ist.fenix.tools.util.i18n.MultiLanguageString;
 
 public class ActionNode extends ActionNode_Base {
 
-    public ActionNode(final VirtualHost virtualHost, final Node node) {
+    public ActionNode() {
 	super();
+    }
+
+    public ActionNode(final VirtualHost virtualHost, final Node node, final String path, final String method,
+	    final String bundle, final String key, final PersistentGroup accessibilityGroup) {
+	super();
+	init(virtualHost, node, path, method, bundle, key, accessibilityGroup);
+    }
+
+    protected void init(final VirtualHost virtualHost, final Node node, final String path, final String method,
+	    final String bundle, final String key, final PersistentGroup accessibilityGroup) {
 	init(virtualHost, node, null);
+	setPath(path);
+	setMethod(method);
+	setLinkBundle(bundle);
+	setLinkKey(key);
+	setAccessibilityGroup(accessibilityGroup);
     }
 
     @Service
     public static ActionNode createActionNode(final VirtualHost virtualHost, final Node node, final String path,
 	    final String method, final String bundle, final String key, final PersistentGroup accessibilityGroup) {
-	final ActionNode actionNode = new ActionNode(virtualHost, node);
-	actionNode.setPath(path);
-	actionNode.setMethod(method);
-	actionNode.setLinkBundle(bundle);
-	actionNode.setLinkKey(key);
-	actionNode.setAccessibilityGroup(accessibilityGroup);
-	return actionNode;
+	return new ActionNode(virtualHost, node, path, method, bundle, key, accessibilityGroup);
     }
 
     @Override
