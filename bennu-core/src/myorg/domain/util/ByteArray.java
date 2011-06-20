aaa35bd5ccb233e31ca3643ce3c8cf52bb8b1e22
Anil Mamade Ali Kassamali <amak@ist.utl.pt>
2011-06-20 13:43:19 +0000

    Bug fix: They shouldnt be commited to trunk so we revert files

--- bennu-core/src/myorg/domain/util/ByteArray.java
+++ bennu-core/src/myorg/domain/util/ByteArray.java
@@ -24,34 +24,18 @@
 
 package myorg.domain.util;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.io.Serializable;
-
-import pt.utl.ist.fenix.tools.util.FileUtils;
 
 public class ByteArray implements Serializable {
 
     private byte[] bytes;
 
-    public ByteArray(final InputStream stream) throws IOException {
-	final ByteArrayOutputStream output = new ByteArrayOutputStream();
-	FileUtils.copy(stream, output);
-	this.bytes = output.toByteArray();
-    }
-    
     public ByteArray(byte[] value) {        
         this.bytes = value;
     }
 
     public byte[] getBytes() {
-	return bytes == null ? null : bytes.clone();
-    }
-
-    public static byte[] toBytes(final InputStream stream) throws IOException {
-	final ByteArray byteArray = new ByteArray(stream);
-	return byteArray.getBytes();
+        return bytes;
     }
 
     public void setBytes(byte[] bytes) {
