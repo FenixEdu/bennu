27bc9177d3d646610ca07ba96a9ad043c805b304
Luis Cruz <luis.cruz@ist.utl.pt>
2012-03-04 22:18:12 +0000

    Minor Change: Applied license and authorship to module.

--- email-dispatch/src/pt/ist/emailNotifier/domain/Email.java
+++ email-dispatch/src/pt/ist/emailNotifier/domain/Email.java
@@ -1,3 +1,27 @@
+/*
+ * @(#)Email.java
+ *
+ * Copyright 2009 Instituto Superior Tecnico
+ * Founding Authors: Luis Cruz
+ * 
+ *      https://fenix-ashes.ist.utl.pt/
+ * 
+ *   This file is part of the E-mail SMTP Adapter Module.
+ *
+ *   The E-mail SMTP Adapter Module is free software: you can
+ *   redistribute it and/or modify it under the terms of the GNU Lesser General
+ *   Public License as published by the Free Software Foundation, either version 
+ *   3 of the License, or (at your option) any later version.
+ *
+ *   The E-mail Module is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public License
+ *   along with the E-mail Module. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
 package pt.ist.emailNotifier.domain;
 
 import java.io.IOException;
@@ -30,6 +54,12 @@ import pt.ist.fenixWebFramework.services.Service;
 import pt.utl.ist.fenix.tools.util.PropertiesManager;
 import pt.utl.ist.fenix.tools.util.StringAppender;
 
+/**
+ * 
+ * @author Luis Cruz
+ * @author Paulo Abrantes
+ * 
+ */
 public class Email extends Email_Base {
 
     private static Session SESSION = null;
--- email-dispatch/src/pt/ist/emailNotifier/domain/ManualDispatcher.java
+++ email-dispatch/src/pt/ist/emailNotifier/domain/ManualDispatcher.java
@@ -1,3 +1,27 @@
+/*
+ * @(#)ManualDispatcher.java
+ *
+ * Copyright 2012 Instituto Superior Tecnico
+ * Founding Authors: Luis Cruz
+ * 
+ *      https://fenix-ashes.ist.utl.pt/
+ * 
+ *   This file is part of the E-mail SMTP Adapter Module.
+ *
+ *   The E-mail SMTP Adapter Module is free software: you can
+ *   redistribute it and/or modify it under the terms of the GNU Lesser General
+ *   Public License as published by the Free Software Foundation, either version 
+ *   3 of the License, or (at your option) any later version.
+ *
+ *   The E-mail Module is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public License
+ *   along with the E-mail Module. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
 package pt.ist.emailNotifier.domain;
 
 import java.util.Collection;
@@ -5,6 +29,11 @@ import java.util.Collection;
 import pt.utl.ist.fenix.tools.smtp.EmailSender;
 import myorg.domain.scheduler.WriteCustomTask;
 
+/**
+ * 
+ * @author Luis Cruz
+ * 
+ */
 public class ManualDispatcher extends WriteCustomTask {
 
     @Override
