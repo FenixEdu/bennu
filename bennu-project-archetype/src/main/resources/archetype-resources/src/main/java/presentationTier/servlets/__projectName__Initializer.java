eeb3a5a2b8785d25727c98c6e4ad6edfafec4ff5
David Martinho <davidmartinho@ist.utl.pt>
2012-07-18 17:46:37 +0000

    Minor change: Made some minor improvements to the bennu project archetype

---
+++ bennu-project-archetype/src/main/resources/archetype-resources/src/main/java/presentationTier/servlets/__projectName__Initializer.java
@@ -0,0 +1,18 @@
+package ${package}.presentationTier.servlets;
+
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+@WebListener
+public class ${projectName}Initializer implements ServletContextListener {
+
+	@Override
+	public void contextDestroyed(ServletContextEvent event) {
+		
+	}
+	
+	@Override
+    public void contextDestroyed(ServletContextEvent event) {
+		
+    }
+}
