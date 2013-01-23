d87e386258afe0d0fa7165f5460fbda9673b0d1f
Pedro Santos <pedro.san7os@gmail.com>
2013-01-23 17:25:14 +0000

    Added jdbc drivers shutdown on core's context listener

--- bennu-core/src/main/java/pt/ist/bennu/core/servlets/BennuCoreContextListener.java
+++ bennu-core/src/main/java/pt/ist/bennu/core/servlets/BennuCoreContextListener.java
@@ -19,11 +19,18 @@
  */
 package pt.ist.bennu.core.servlets;
 
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Enumeration;
 import java.util.Locale;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import pt.ist.bennu.core.domain.Bennu;
 import pt.ist.bennu.core.domain.VirtualHost;
@@ -42,6 +49,10 @@ import pt.ist.fenixframework.pstm.PersistentRoot;
  */
 @WebListener
 public class BennuCoreContextListener implements ServletContextListener {
+	private static final Logger logger = LoggerFactory.getLogger(BennuCoreContextListener.class);
+
+	private ClassLoader thisClassLoader;
+
 	@Override
 	public void contextInitialized(ServletContextEvent event) {
 		try {
@@ -68,11 +79,48 @@ public class BennuCoreContextListener implements ServletContextListener {
 
 	@Override
 	public void contextDestroyed(ServletContextEvent event) {
+		this.thisClassLoader = this.getClass().getClassLoader();
+		interruptThreads();
+		deregisterJDBCDrivers();
 	}
 
 	private void setLocale() {
 		final String language = ConfigurationManager.getProperty("language");
 		final String location = ConfigurationManager.getProperty("location");
 		Language.setLocale(new Locale(language, location));
+	}
+
+	private void deregisterJDBCDrivers() {
+
+		Enumeration<Driver> drivers = DriverManager.getDrivers();
+
+		while (drivers.hasMoreElements()) {
+			Driver driver = drivers.nextElement();
+
+			ClassLoader loader = driver.getClass().getClassLoader();
+
+			if (loader != null && loader.equals(this.thisClassLoader)) {
+				try {
+					DriverManager.deregisterDriver(driver);
+					System.out.println("Successfully deregistered JDBC driver " + driver);
+				} catch (SQLException e) {
+					logger.warn("Failed to deregister JDBC driver " + driver + ". This may cause a potential leak.", e);
+				}
+			}
+
+		}
+	}
+
+	private void interruptThreads() {
+
+		for (Thread thread : Thread.getAllStackTraces().keySet()) {
+			if (thread == null || thread.getContextClassLoader() != thisClassLoader || thread == Thread.currentThread())
+				continue;
+
+			if (thread.isAlive()) {
+				System.out.println("Killing initiated thread: " + thread + " of class " + thread.getClass());
+				thread.interrupt();
+			}
+		}
 	}
 }
