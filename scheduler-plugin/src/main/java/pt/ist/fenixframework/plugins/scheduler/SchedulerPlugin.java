80fda2a21d99f4292ca634addf9b5c8a8579869f
Pedro Santos <pedro.miguel.santos@ist.utl.pt>
2013-01-31 20:35:53 +0000

    Reformatted and enforced code conventions

--- scheduler-plugin/src/main/java/pt/ist/fenixframework/plugins/scheduler/SchedulerPlugin.java
+++ scheduler-plugin/src/main/java/pt/ist/fenixframework/plugins/scheduler/SchedulerPlugin.java
@@ -14,28 +14,28 @@ import pt.ist.fenixframework.plugins.scheduler.domain.SchedulerSystem;
 
 @WebListener
 public class SchedulerPlugin implements FenixFrameworkPlugin, ServletContextListener {
-    private static boolean initialized = false;
+	private static boolean initialized = false;
 
-    @Override
-    public List<URL> getDomainModel() {
-	return Collections.singletonList(getClass().getResource("/scheduler-plugin.dml"));
-    }
-
-    @Override
-    @Service
-    public void initialize() {
-	if (!initialized) {
-	    SchedulerSystem.getInstance();
-	    initialized = true;
+	@Override
+	public List<URL> getDomainModel() {
+		return Collections.singletonList(getClass().getResource("/scheduler-plugin.dml"));
 	}
-    }
 
-    @Override
-    public void contextInitialized(ServletContextEvent event) {
-	initialize();
-    }
+	@Override
+	@Service
+	public void initialize() {
+		if (!initialized) {
+			SchedulerSystem.getInstance();
+			initialized = true;
+		}
+	}
 
-    @Override
-    public void contextDestroyed(ServletContextEvent event) {
-    }
+	@Override
+	public void contextInitialized(ServletContextEvent event) {
+		initialize();
+	}
+
+	@Override
+	public void contextDestroyed(ServletContextEvent event) {
+	}
 }
--- scheduler-plugin/src/main/java/pt/ist/fenixframework/plugins/scheduler/domain/TaskLog.java
+++ scheduler-plugin/src/main/java/pt/ist/fenixframework/plugins/scheduler/domain/TaskLog.java
@@ -1,9 +1,11 @@
 package pt.ist.fenixframework.plugins.scheduler.domain;
 
+import dml-maven-plugin.pt.ist.fenixframework.plugins.scheduler.domain.TaskLog_Base;
+
 public class TaskLog extends TaskLog_Base {
-    
-    public  TaskLog() {
-        super();
-    }
-    
+
+	public TaskLog() {
+		super();
+	}
+
 }
