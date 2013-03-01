3be903a5b65fb95221a30ccaca6459bd47068ad2
sergiofbsilva <sergiofbsilva@gmail.com>
2013-03-01 01:26:26 +0000

    Each rest resource is mapped within /api/<module-name>/<Path-Value>

--- bennu-core/src/main/java/pt/ist/bennu/core/servlets/BennuJerseyRestApplication.java
+++ bennu-core/src/main/java/pt/ist/bennu/core/servlets/BennuJerseyRestApplication.java
@@ -2,8 +2,13 @@ package pt.ist.bennu.core.servlets;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.sun.jersey.api.core.ScanningResourceConfig;
 import com.sun.jersey.core.spi.scanning.Scanner;
@@ -12,7 +17,9 @@ import com.sun.jersey.core.spi.scanning.ScannerListener;
 
 public class BennuJerseyRestApplication extends ScanningResourceConfig {
     private static Set<Class<?>> types = new HashSet<>();
-
+    private static Map<String,Object> modulePathResource = new HashMap<>();
+    private static Logger LOG = LoggerFactory.getLogger(BennuJerseyRestApplication.class);
+    
     public BennuJerseyRestApplication() {
         super();
         init(new Scanner() {
@@ -29,10 +36,22 @@ public class BennuJerseyRestApplication extends ScanningResourceConfig {
                 }
             }
         });
+        LOG.info("Registering Bennu REST entrypoints:");
     }
 
-    public static void registerEndpoint(Class<?> type) {
+    public static void register(Class<?> type) {
         types.add(type);
     }
+    
+    public Map<String, Object> getExplicitRootResources() {
+    	return modulePathResource;
+    }
+    
+    public static void registerEndpoint(String moduleName, String path, Class<?> type) {
+    	final String completePath = path.startsWith("/") ? moduleName.concat(path) : moduleName.concat("/").concat(path);
+    	modulePathResource.put(completePath, type);
+    	LOG.info("\t{} mapped on '{}'", type.getName(), completePath);
+    }
+    
 
 }
