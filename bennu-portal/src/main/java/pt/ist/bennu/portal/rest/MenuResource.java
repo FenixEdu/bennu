834a3ed23628c49a6b674253dc463372a38cf97a
sergiofbsilva <sergiofbsilva@gmail.com>
2013-03-01 17:04:03 +0000

    Add helper method to get virtual hosts by hostname

--- bennu-portal/src/main/java/pt/ist/bennu/portal/rest/MenuResource.java
+++ bennu-portal/src/main/java/pt/ist/bennu/portal/rest/MenuResource.java
@@ -7,7 +7,9 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response.Status;
 
 import pt.ist.bennu.core.domain.Bennu;
 import pt.ist.bennu.core.domain.VirtualHost;
@@ -17,13 +19,22 @@ import pt.ist.bennu.portal.domain.exception.MenuNotAvailableException;
 import pt.ist.bennu.service.Service;
 
 @Path("menu")
-public class MenuResource extends PortalResource {
+public class MenuResource extends BennuRestResource {
 
     @Path("{hostname: [a-zA-Z0-9\\.]+}")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public String menu(@PathParam("hostname") final String hostname) {
         return view(getVirtualHost(hostname).getMenu());
+    }
+
+    private VirtualHost getVirtualHost(String hostname) {
+        final VirtualHost virtualHost = Bennu.getInstance().getVirtualHost(hostname);
+        if (virtualHost != null) {
+            return virtualHost;
+        } else {
+            throw new WebApplicationException(Status.NOT_FOUND);
+        }
     }
 
     @Path("{hostname: [a-zA-Z0-9\\.]+}")
