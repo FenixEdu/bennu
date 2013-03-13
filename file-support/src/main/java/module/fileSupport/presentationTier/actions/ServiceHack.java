1c468aa440bfb02b1b09dd1c16536770db999eb8
Joao Carvalho <joao.pedro.carvalho@ist.utl.pt>
2013-03-13 17:19:38 +0000

    [File Support] Refactor to Fenix Framework 2 - Refactored to use the Fenix Framework 2 API

--- file-support/src/main/java/module/fileSupport/presentationTier/actions/ServiceHack.java
+++ file-support/src/main/java/module/fileSupport/presentationTier/actions/ServiceHack.java
@@ -27,7 +27,7 @@ package module.fileSupport.presentationTier.actions;
 import module.fileSupport.dto.DBStorageDTO;
 import module.fileSupport.dto.DomainStorageDTO;
 import module.fileSupport.dto.LocalFileSystemStorageDTO;
-import pt.ist.fenixWebFramework.services.Service;
+import pt.ist.fenixframework.Atomic;
 import pt.ist.fenixframework.plugins.fileSupport.domain.FileStorage;
 
 /**
@@ -37,18 +37,18 @@ import pt.ist.fenixframework.plugins.fileSupport.domain.FileStorage;
  */
 public class ServiceHack {
 
-    @Service
+    @Atomic(speculativeReadOnly = false)
     public static void createDomainStorage(DomainStorageDTO domainStorageDTO) {
         FileStorage.createNewDomainStorage(domainStorageDTO.getName());
     }
 
-    @Service
+    @Atomic(speculativeReadOnly = false)
     public static void createDBStorage(DBStorageDTO dbStorageDTO) {
         FileStorage.createNewDatabaseStorage(dbStorageDTO.getName(), dbStorageDTO.getHost(), dbStorageDTO.getPort(),
                 dbStorageDTO.getDbName(), dbStorageDTO.getTableName(), dbStorageDTO.getUsername(), dbStorageDTO.getPassword());
     }
 
-    @Service
+    @Atomic(speculativeReadOnly = false)
     public static void createLocalFileSystemStorage(LocalFileSystemStorageDTO fdStorageDTO) {
         FileStorage.createNewFileSystemStorage(fdStorageDTO.getName(), fdStorageDTO.getPath(),
                 fdStorageDTO.getTreeDirectoriesNameLength());
