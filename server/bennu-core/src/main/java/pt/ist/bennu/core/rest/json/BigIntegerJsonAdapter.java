316cd2dd07c4b9083ddd249254b1ec95be117d74
Pedro Santos <pedro.san7os@gmail.com>
2013-08-22 15:48:44 +0100

    Added BigInteger json adapter

---
+++ server/bennu-core/src/main/java/pt/ist/bennu/core/rest/json/BigIntegerJsonAdapter.java
@@ -0,0 +1,24 @@
+package pt.ist.bennu.core.rest.json;
+
+import java.math.BigInteger;
+
+import pt.ist.bennu.core.annotation.DefaultJsonAdapter;
+import pt.ist.bennu.json.JsonBuilder;
+import pt.ist.bennu.json.JsonCreator;
+import pt.ist.bennu.json.JsonViewer;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonPrimitive;
+
+@DefaultJsonAdapter(BigInteger.class)
+public class BigIntegerJsonAdapter implements JsonViewer<BigInteger>, JsonCreator<BigInteger> {
+    @Override
+    public BigInteger create(JsonElement json, JsonBuilder ctx) {
+        return new BigInteger(json.getAsString());
+    }
+
+    @Override
+    public JsonElement view(BigInteger obj, JsonBuilder ctx) {
+        return new JsonPrimitive(obj.toString());
+    }
+}
