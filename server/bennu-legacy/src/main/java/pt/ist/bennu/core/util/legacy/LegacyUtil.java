c98883bd1c6049db5277b0140debf3d6e51fd6cd
sergiofbsilva <sergiofbsilva@gmail.com>
2013-08-28 14:57:59 +0100

    Add legacy method for roletypes as string

--- server/bennu-legacy/src/main/java/pt/ist/bennu/core/util/legacy/LegacyUtil.java
+++ server/bennu-legacy/src/main/java/pt/ist/bennu/core/util/legacy/LegacyUtil.java
@@ -8,10 +8,29 @@ import pt.ist.bennu.core.domain.groups.legacy.Role;
 public class LegacyUtil {
 
     public static boolean hasRoleType(final User user, final RoleType roleType) {
+        if (user == null) {
+            return false;
+        }
         for (final People people : user.getPeopleGroupsSet()) {
             if (people instanceof Role) {
                 final Role role = (Role) people;
                 if (role.isRole(roleType) && role.isMember(user)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static boolean hasRoleType(final User user, final String roleAsString) {
+        if (user == null) {
+            return false;
+        }
+
+        for (final People people : user.getPeopleGroupsSet()) {
+            if (people instanceof Role) {
+                final Role role = (Role) people;
+                if (role.getGroupName().equals(roleAsString) && role.isMember(user)) {
                     return true;
                 }
             }
