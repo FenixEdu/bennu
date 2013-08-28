aa95fda7db3c8d0080e51b5e8b7e13b1082349e1
sergiofbsilva <sergiofbsilva@gmail.com>
2013-08-28 15:43:15 +0100

    Move money renderers to bennu-legacy

---
+++ server/bennu-legacy/src/main/java/pt/ist/bennu/core/presentationTier/renderers/MoneyRenderer.java
@@ -0,0 +1,59 @@
+/* 
+* @(#)MoneyRenderer.java 
+* 
+* Copyright 2010 Instituto Superior Tecnico 
+* Founding Authors: João Figueiredo, Luis Cruz, Paulo Abrantes, Susana Fernandes 
+*  
+*      https://fenix-ashes.ist.utl.pt/ 
+*  
+*   This file is part of the Bennu Web Application Infrastructure. 
+* 
+*   The Bennu Web Application Infrastructure is free software: you can 
+*   redistribute it and/or modify it under the terms of the GNU Lesser General 
+*   Public License as published by the Free Software Foundation, either version  
+*   3 of the License, or (at your option) any later version. 
+* 
+*   Bennu is distributed in the hope that it will be useful, 
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of 
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+*   GNU Lesser General Public License for more details. 
+* 
+*   You should have received a copy of the GNU Lesser General Public License 
+*   along with Bennu. If not, see <http://www.gnu.org/licenses/>. 
+*  
+*/
+package pt.ist.bennu.core.presentationTier.renderers;
+
+import pt.ist.bennu.core.domain.util.Money;
+import pt.ist.fenixWebFramework.renderers.OutputRenderer;
+import pt.ist.fenixWebFramework.renderers.components.HtmlComponent;
+import pt.ist.fenixWebFramework.renderers.components.HtmlInlineContainer;
+import pt.ist.fenixWebFramework.renderers.components.HtmlText;
+import pt.ist.fenixWebFramework.renderers.layouts.Layout;
+
+/**
+ * 
+ * @author Susana Fernandes
+ * 
+ */
+public class MoneyRenderer extends OutputRenderer {
+
+    @Override
+    protected Layout getLayout(Object object, Class type) {
+        return new Layout() {
+
+            @Override
+            public HtmlComponent createComponent(Object object, Class type) {
+                Money money = (Money) object;
+                HtmlText text = new HtmlText(money.toFormatString());
+                HtmlInlineContainer container = new HtmlInlineContainer();
+                container.addChild(text);
+
+                return container;
+            }
+
+        };
+
+    }
+
+}
