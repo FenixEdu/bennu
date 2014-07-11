930791281ba5a9889b97a72dc7aa10d8f2567dcb
João Carvalho <joao.pedro.carvalho@tecnico.ulisboa.pt>
2014-07-11 18:42:24 +0100

    [bennu-core] Added a static cache filter

---
+++ server/bennu-core/src/main/java/org/fenixedu/bennu/core/filters/StaticCacheFilter.java
@@ -0,0 +1,48 @@
+package org.fenixedu.bennu.core.filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
+
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+/**
+ * Filter that adds caching headers to static resources.
+ * 
+ * These headers tell the browsers to locally cache resources for 12 hours.
+ * 
+ * @author João Carvalho (joao.pedro.carvalho@tecnico.ulisboa.pt)
+ *
+ */
+@WebFilter(urlPatterns = { "*.css", "*.js", "*.gif", "*.png", "*.jpg", "*.jpeg", "*.woff", "*.svg" })
+public class StaticCacheFilter implements Filter {
+
+    private final DateTimeFormatter formatter = DateTimeFormat.forPattern("E, d MMM yyyy HH:mm:ss z");
+
+    @Override
+    public void init(final FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(final ServletRequest request, final ServletResponse response, final FilterChain chain)
+            throws IOException, ServletException {
+        final HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+        httpServletResponse.setHeader("Expires", formatter.print(DateTime.now().plusHours(12)));
+        httpServletResponse.setHeader("Cache-Control", "max-age=43200");
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+    }
+
+}
