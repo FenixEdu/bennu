b9ba514d5d46365cee4d0d1600c96661cea05b2b
João Carvalho <joao.pedro.carvalho@tecnico.ulisboa.pt>
2014-07-14 11:44:46 +0100

    [default-theme] Cleanup: Using logo and locale APIs directly

--- theme/default/src/main/webapp/themes/default/js/scripts.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-        $(".lang").click(function(e) {
-                if(e.target.parentElement.classList.contains("disabled")) { return; }
-                var lang = $(e.target).data("lang").replace('_', '-');
-                $.post(window.contextPath + "/api/bennu-core/profile/locale/" + lang, null, function() {
-                        location.reload();
-                });
-        });
-});
