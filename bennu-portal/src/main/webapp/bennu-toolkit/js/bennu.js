828c423559492af6ba21abe79bb8273b74225bf5
Artur Ventura <artur.ventura@gmail.com>
2014-10-06 17:10:05 +0100

    Adding Bennu Toolkit to Bennu Portal

---
+++ bennu-portal/src/main/webapp/bennu-toolkit/js/bennu.js
@@ -0,0 +1,224 @@
+(function () {
+
+    var cookies = null;
+
+    function readCookie(name, c, C, i) {
+        if (cookies) {
+            return cookies[name];
+        }
+
+        c = document.cookie.split('; ');
+        cookies = {};
+
+        for (i = c.length - 1; i >= 0; i--) {
+            C = c[i].split('=');
+            cookies[C[0]] = C[1];
+        }
+
+        return cookies[name];
+    }
+
+    window.Bennu = function () {
+        return $.apply($, arguments);
+    };
+
+    Bennu.version = "1.0.0";
+
+    Bennu.toString = function () {
+        return "Bennu Toolkit v" + Bennu.version;
+    };
+
+    Bennu.gensym = function () {
+        var text = "";
+        var possible = "abcdefghijklmnopqrstuvwxyz0123456789";
+
+        for (var i = 0; i < 5; i++) {
+            text += possible.charAt(Math.floor(Math.random() * possible.length));
+        }
+
+        return text;
+    };
+
+    Bennu.contextPath = readCookie("contextPath") || "";
+
+    Bennu.developmentMode = (readCookie("developmentMode") && true) || false;
+
+    function prefixForEvent(a) {
+        a[0] = a[0].split(" ").map(function (e) {
+            return "bennu-toolkit-" + e;
+        }).reduce(function (x, y) {
+            return x + " " + y;
+        });
+    }
+
+    Bennu.on = function () {
+        prefixForEvent(arguments);
+        var q = $("html");
+        q.on.apply(q, arguments);
+    };
+
+    Bennu.off = function () {
+        prefixForEvent(arguments);
+        var q = $("html");
+        q.off.apply(q, arguments);
+    };
+
+    Bennu.trigger = function () {
+        prefixForEvent(arguments);
+        var q = $("html");
+        q.trigger.apply(q, arguments);
+    };
+
+    Bennu.addMls = function (model) {
+        var completeLanguage = this.locale.tag;
+        var currentLanguage = completeLanguage;
+        var langs = this.locales;
+        model['_mls'] = function () {
+            return function (val) {
+                if (typeof val === "string") {
+                    val = this[val];
+                }
+                if (val) {
+                    if (val[completeLanguage]) {
+                        return val[completeLanguage];
+                    }
+                    currentLanguage = BennuPortal.lang;
+                    if (val[currentLanguage]) {
+                        return val[currentLanguage];
+                    }
+
+                    //search for other specific currentLanguage
+                    for (var lang in val) {
+                        if (lang.indexOf(currentLanguage) === 0) {
+                            return val[lang];
+                        }
+                    }
+                    var fallbackLanguage = undefined;
+                    $(langs).each(function () {
+                        var eachlang = this.tag;
+                        if (eachlang != completeLanguage && eachlang.indexOf(currentLanguage) === 0) {
+                            fallbackLanguage = eachlang;
+                            return false;
+                        }
+                    });
+                    if (fallbackLanguage != undefined && val[fallbackLanguage] != undefined) {
+                        return val[fallbackLanguage];
+                    }
+                    // Fallback, return the first key in the object...
+                    return val[Object.keys(val)[0]];
+                }
+                return "_mls!!" + val + "!!";
+            };
+        };
+        model["_lang"] = currentLanguage;
+    };
+
+    Bennu.login = function (user, pass, callback) {
+        $.post(Bennu.contextPath + "/api/bennu-core/profile/login", {
+            username: user,
+            password: pass
+        }, function (data, textStatus, jqxhr) {
+            callback(data, textStatus, jqxhr);
+        });
+    };
+
+    Bennu.logout = function (callback) {
+        var logoutUrl = Bennu.contextPath + "/api/bennu-core/profile/logout";
+        if (hostJson.casEnabled) {
+            logoutUrl = hostJson.logoutUrl;
+        }
+        $.get(logoutUrl, null, function (data, textStatus, jqxhr) {
+            callback(data, textStatus, jqxhr);
+        });
+    };
+
+    Bennu.changeLanguage = function (tag, callback) {
+        $.post(Bennu.contextPath + "/api/bennu-core/profile/locale/" + tag, null, function (data, textStatus, jqxhr) {
+            callback(data, textStatus, jqxhr);
+        });
+    };
+
+    Bennu.load = function (scripts) {
+        $(scripts).each(function (i, e) {
+            $.getScript(Bennu.contextPath + e, function () {
+            });
+        });
+    };
+
+    Bennu.widgetHandler = Bennu.widgetHandler || {}
+    Bennu.widgetHandler.makeFor = function (e, onremove){
+        var events = [];
+        var result = {
+            get:function(){
+                return $(e).val();
+            },
+
+            set:function(val){
+                $(e).val(val);
+                $(e).trigger("change");
+            },
+
+            clear:function(val){
+                this.set("");
+            },
+
+            onchange:function(fn){
+                events.push(fn);
+            },
+
+            trigger: function(){
+                for (var i = 0; i < events.length; i++) {
+                    events[i].apply(e,{
+                        type:"change",
+                        target:e
+                    });
+                }
+            },
+
+            remove: function(){
+                $(e).data("input").remove();
+                $(e).data("input", null);
+                $(e).data("handler", null);
+                $(e).off(".bennu");
+            }
+        }
+
+        $(e).data("handler", result);
+        return result;
+    };
+
+    //TODO: this needs to go, prefering head meta keys;
+    setTimeout(function () {
+        $.ajax({
+            type: "GET",
+            url: Bennu.contextPath + "/api/bennu-portal/data",
+            async: false,
+            dataType: "json",
+            success: function (hostJson, status, response) {
+                Bennu.username = hostJson.username;
+                Bennu.locales = hostJson.locales;
+                Bennu.locale = hostJson.locale;
+                Bennu.groups = hostJson.groups;
+                Bennu.lang = (function (locale) {
+                    if (locale.indexOf("-") != -1) {
+                        return locale.split("-")[0];
+                    }
+                    return locale;
+                })(hostJson.locale.tag);
+                Bennu.trigger("load");
+            },
+            error: function (xhr, status, errorThrown) {
+                Bennu.username = null;
+                Bennu.locales = [
+                    {displayName: "English (United Kingdom)", lang: "en", tag: "en-GB"}
+                ];
+                Bennu.locale = {displayName: "English (United Kingdom)", lang: "en", tag: "en-GB"};
+                Bennu.groups = null;
+                Bennu.lang = "en";
+                Bennu.trigger("load");
+
+            }
+        })
+    }, 100);
+    ;
+}).call(this);
---
+++ bennu-portal/src/main/webapp/bennu-toolkit/js/localizedString.js
@@ -0,0 +1,116 @@
+(function () {
+    Bennu.localizedString = Bennu.localizedString || {};
+    Bennu.localizedString.attr = "bennu-localized-string";
+
+    Bennu.localizedString.changeData = function (locale, localeButton, inputField, widget) {
+        widget = $(widget);
+        var val = $(widget.data("related")).val();
+
+        if (val === "") {
+            $(widget.data("related")).val("{}");
+            val = "{}";
+        }
+        var val = JSON.parse(val)[locale.tag]
+        inputField.val(val || "");
+        localeButton.html(locale.displayName || locale.tag);
+        localeButton.data("locale", locale);
+        widget.data("locale", locale);
+    };
+
+    Bennu.localizedString.updateValueForLanguage = function (input, localeContainer, widget) {
+        var data = JSON.parse($(widget.data("related")).val());
+        data[localeContainer.data("locale").tag] = input.val();
+        $(widget.data("related")).val(JSON.stringify(data));
+        $(widget.data("related")).trigger("change");
+    };
+
+    Bennu.localizedString.makeLocaleList = function (menu, widget, callback) {
+        for (var i = 0; i < Bennu.locales.length; i++) {
+            var locale = Bennu.locales[i];
+            var menuOption = $("<li><a href='#' onclick='return false;' >" + (locale.displayName || locale.tag) + "</a>");
+            menuOption.data("locale", locale);
+            menuOption.on("click", callback);
+            menu.append(menuOption);
+        }
+    };
+
+    Bennu.localizedString.getContent = function (input, locale) {
+        if (!input || typeof input === 'string') return input;
+
+        if (locale && typeof locale !== "string"){
+            var tag = locale.tag;
+        }else if(!locale){
+            var tag = Bennu.locale.tag;
+        }else{
+            var tag = locale
+        }
+
+        if (tag in input) {
+            return input[tag];
+        }
+        tag = tag.split('-')[0];
+        if (tag in input) {
+            return input[tag];
+        }
+        for (var lang in input) {
+            if (lang.indexOf(tag) === 0) {
+                return input[lang];
+            }
+        }
+        return input[Object.keys(input)[0]];
+    };
+
+    Bennu.localizedString.createWidget = function (input) {
+        input = $(input);
+
+        var attr = input.attr("bennu-html-editor");
+        if (attr !== null && attr !== undefined) {
+            return;
+        }
+
+        if (!$(input).data("input")) {
+
+            if (input.prop("tagName") == "INPUT") {
+                var widget = $('<div class="bennu-localized-string-input-group" >' +
+                    '<div class="input-group"><input type="text" class="form-control bennu-localized-string-input">' +
+                    '<div class="input-group-btn bennu-localized-string-group">' +
+                    '<button type="button" class="hidden-sm hidden-xs btn btn-default dropdown-toggle bennu-localized-string-button" data-toggle="dropdown">' +
+                    '<span class="bennu-localized-string-language"></span> <span class="caret"></span></button>' +
+                    '<button type="button" class="visible-sm visible-xs btn btn-default dropdown-toggle bennu-localized-string-button" data-toggle="dropdown">' +
+                    '<span class="glyphicon glyphicon-globe"></span> <span class="caret"></span></button>' +
+                    '</button>' +
+                    '<ul class="dropdown-menu bennu-localized-string-menu pull-right" role="menu"></ul></div></div>' +
+                    '<p class="help-block"></p></div>');
+            } else if (input.prop("tagName") == "TEXTAREA") {
+                var widget = $('<div class="bennu-localized-string-textArea"><p><div class="btn-group bennu-localized-string-group"><button type="button" class="btn btn-default dropdown-toggle bennu-localized-string-button" data-toggle="dropdown"><span class="bennu-localized-string-language"></span><span class="caret"></span></button><ul class="dropdown-menu bennu-localized-string-menu" role="menu"></ul></div></p><p><textarea class="form-control bennu-localized-string-textarea"></textarea><p class="help-block"></p></div>');
+            }
+
+            widget.data("related", input);
+            Bennu.localizedString.makeLocaleList($(".bennu-localized-string-menu", widget), widget, function (e) {
+                Bennu.localizedString.changeData($(e.target).parent().data("locale"), $(".bennu-localized-string-language", widget), $(".bennu-localized-string-input,.bennu-localized-string-textarea", widget), widget);
+            });
+
+            Bennu.localizedString.changeData(Bennu.locale, $(".bennu-localized-string-language", widget), $(".bennu-localized-string-input,.bennu-localized-string-textarea", widget), widget);
+            $(".bennu-localized-string-input,.bennu-localized-string-textarea", widget).on("input propertychange", function () {
+
+                Bennu.localizedString.updateValueForLanguage($(".bennu-localized-string-input,.bennu-localized-string-textarea", widget), $(".bennu-localized-string-language", widget), widget);
+
+                $(".help-block", widget).empty();
+                widget.removeClass("has-error");
+            });
+
+            input.data("input", widget);
+            input.on("change.bennu", function (e) {
+                Bennu.localizedString.changeData(widget.data("locale"), $(".bennu-localized-string-language", widget), $(".bennu-localized-string-input,.bennu-localized-string-textarea", widget), widget);
+                input.data("handler").trigger();
+            });
+
+            input.after(widget);
+            Bennu.validation.attachToForm(widget);
+
+            return Bennu.widgetHandler.makeFor(input);
+        }
+    };
+
+
+})();
