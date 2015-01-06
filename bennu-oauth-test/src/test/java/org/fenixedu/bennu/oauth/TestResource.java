771be94ba0341b8d78263792e3c7dc003f631e84
sergiofbsilva <sergiofbsilva@gmail.com>
2015-01-06 11:09:01 +0000

    Add licensing headers + additional pom information

--- bennu-oauth-test/src/test/java/org/fenixedu/bennu/oauth/TestResource.java
+++ bennu-oauth-test/src/test/java/org/fenixedu/bennu/oauth/TestResource.java
@@ -1,3 +1,21 @@
+/**
+ * Copyright © 2015 Instituto Superior Técnico
+ *
+ * This file is part of Bennu OAuth Test.
+ *
+ * Bennu OAuth Test is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Bennu OAuth Test is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Bennu OAuth Test.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.fenixedu.bennu.oauth;
 
 import javax.ws.rs.GET;
