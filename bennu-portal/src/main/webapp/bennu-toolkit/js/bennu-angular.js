49f2ce9f1076b4eac6395022a5240ce6d820f09c
João Carvalho <joao.pedro.carvalho@tecnico.ulisboa.pt>
2015-05-14 19:02:21 +0100

    [toolkit] Added datetime components to the angular toolkit
      * Issue: BNN-205

--- bennu-portal/src/main/webapp/bennu-toolkit/js/bennu-angular.js
+++ bennu-portal/src/main/webapp/bennu-toolkit/js/bennu-angular.js
@@ -31,6 +31,35 @@
 	  }
 	}]);
 
+	function toolkitDirective(name, widgetProvider) {
+		bennuToolkit.directive(name, ['$timeout', function($timeout) {
+		  return {
+		    restrict: 'A',
+		    scope: {
+		      model: '=' + name
+		    },
+		    link: function(scope, el, attr) {
+		      el.hide();
+		      var handler = widgetProvider(el);
+		      scope.$watch('model', function(value) {
+		        if(value !== handler.get()) {
+		          handler.set(value);
+		        }
+		      });
+		      handler.onchange(function () {
+		        $timeout(function () {
+		          scope.model = handler.get();
+		        });
+		      });
+		    }
+		  }
+		}]);
+	}
+
+	toolkitDirective('bennuDateTime', Bennu.datetime.createDateTimeWidget);
+	toolkitDirective('bennuDate', Bennu.datetime.createDateWidget);
+	toolkitDirective('bennuTime', Bennu.datetime.createTimeWidget);
+
     bennuToolkit.directive('ngHtmlEditor', ['$timeout', function($timeout) {
         return {
             restrict: 'A',
