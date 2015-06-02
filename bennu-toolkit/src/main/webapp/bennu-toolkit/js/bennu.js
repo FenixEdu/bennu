2e5539231e1b3fcd5b5735ba3fae50efbe26d4df
Artur Ventura <artur.ventura@gmail.com>
2015-06-03 00:01:28 +0100

    Adds licences to files. Also, renderable compressed copyright.

--- bennu-toolkit/src/main/webapp/bennu-toolkit/js/bennu.js
+++ bennu-toolkit/src/main/webapp/bennu-toolkit/js/bennu.js
@@ -1,3 +1,25 @@
+/*! 
+ * Bennu Toolkit
+ * https://github.com/FenixEdu/bennu
+ * Copyright (c) 2014 Instituto Superior Técnico
+ */
+/*
+ * bennu.js
+ * 
+ * Copyright (c) 2014, Instituto Superior Técnico. All rights reserved.
+ * 
+ * This file is part of Bennu Toolkit.
+ * 
+ * Bennu Toolkit is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * 
+ * Bennu Toolkit is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with Bennu Toolkit. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
 (function () {
 
     window.Bennu = window.Bennu || {};
