59364b60f2d4752f9a514e72cee49e99f18a65c5
sergiofbsilva <sergiofbsilva@gmail.com>
2015-06-22 14:36:14 +0100

    Return null when external id parsing fails.
    Issue: BNN-250

--- bennu-test/src/test/java/pt/ist/fenixframework/backend/jvstm/InMemDomainObjectValidBackEnd.java
+++ bennu-test/src/test/java/pt/ist/fenixframework/backend/jvstm/InMemDomainObjectValidBackEnd.java
@@ -4,6 +4,8 @@ import pt.ist.fenixframework.DomainObject;
 import pt.ist.fenixframework.FenixFramework;
 import pt.ist.fenixframework.backend.jvstm.repository.NoRepository;
 
+import com.google.common.base.Strings;
+
 /***
  * 
  * This backend is necessary since {@link JVSTMBackEnd} throws {@link UnsupportedOperationException} when invoking
@@ -24,4 +26,15 @@ class InMemDomainObjectValidBackEnd extends JVSTMBackEnd {
         return object != null;
     }
 
+    @Override
+    public <T extends DomainObject> T getDomainObject(String externalId) {
+        if (Strings.isNullOrEmpty(externalId)) {
+            return null;
+        }
+        try {
+            return super.getDomainObject(externalId);
+        } catch (NumberFormatException e) {
+            return null;
+        }
+    }
 }
