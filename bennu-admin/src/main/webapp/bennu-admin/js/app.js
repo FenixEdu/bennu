4d7d7ae55e842f0793b27499fd55ad2c2b9cff90
Pedro Santos <pedro.san7os@gmail.com>
2015-07-29 10:26:47 +0100

    Find user rest api only includes active users by default but can be parameterized to include all.
     * Also changed where appropriate the invocation to request the inactive as well

--- bennu-admin/src/main/webapp/bennu-admin/js/app.js
+++ bennu-admin/src/main/webapp/bennu-admin/js/app.js
@@ -577,7 +577,7 @@ app.controller('UserManagementController', [ '$scope', '$http', '$state', '$loca
   $scope.doSearch = function() {
     $scope.currentSearch = $scope.query;
     if($scope.query) {
-      $http.post(contextPath + '/api/bennu-core/users/find?query=' + $scope.query).success(function (data) {
+      $http.post(contextPath + '/api/bennu-core/users/find?includeInactive=true&query=' + $scope.query).success(function (data) {
         $scope.users = data.users;
       });   
     } else {
