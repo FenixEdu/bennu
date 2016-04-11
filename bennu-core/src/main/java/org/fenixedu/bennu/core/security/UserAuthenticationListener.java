8b8f34160254971e0d8f068f5baec21cabf43e63
João Carvalho <joaopecarvalho@gmail.com>
2016-04-11 13:32:47 +0100

    UserAuthenticationListener now receives the Request and Response
      * Issue: BNN-300

--- bennu-core/src/main/java/org/fenixedu/bennu/core/security/UserAuthenticationListener.java
+++ bennu-core/src/main/java/org/fenixedu/bennu/core/security/UserAuthenticationListener.java
@@ -1,17 +1,18 @@
 package org.fenixedu.bennu.core.security;
 
-import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.fenixedu.bennu.core.domain.User;
 
 /**
  * Listener of user login/logout events. Runs synchronously with the login.
- * 
+ *
  * @see Authenticate#addUserAuthenticationListener(UserAuthenticationListener)
  * @see Authenticate#removeUserAuthenticationListener(UserAuthenticationListener)
  */
 public interface UserAuthenticationListener {
-    public void onLogin(HttpSession session, User user);
+    void onLogin(HttpServletRequest request, HttpServletResponse response, User user);
 
-    public void onLogout(HttpSession session, User user);
+    void onLogout(HttpServletRequest request, HttpServletResponse response, User user);
 }
