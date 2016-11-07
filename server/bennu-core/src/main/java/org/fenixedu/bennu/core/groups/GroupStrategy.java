4e89d73bac0d1207bc318fae700ecb7d019ad995
Luis Cruz <luis.cruz@tecnico.pt>
2016-11-07 17:29:14 +0000

    Released version 3.4.3

--- server/bennu-core/src/main/java/org/fenixedu/bennu/core/groups/GroupStrategy.java
+++ server/bennu-core/src/main/java/org/fenixedu/bennu/core/groups/GroupStrategy.java
@@ -31,7 +31,7 @@ public abstract class GroupStrategy extends CustomGroup {
      * Retrieves a {@link GroupStrategy} by the name of its type.
      * 
      * If the given type is no longer available (or the typeName is invalid), it returns a strategy that behaves the same as
-     * {@link NobodyGrou}.
+     * {@link NobodyGroup}.
      */
     public static GroupStrategy strategyForType(String typeName) {
         try {
