5577c77584ef7ee1dea6891b5bd943cc19931946
JNPA <joao.amaral@qub-it.com>
2017-10-26 18:56:07 +0100

    Update jquery to 3.2.1

--- bennu-toolkit/src/main/webapp/bennu-toolkit/js/mutations.js
+++ bennu-toolkit/src/main/webapp/bennu-toolkit/js/mutations.js
@@ -97,7 +97,7 @@
         // ------
         var inputs = $("[" + Bennu.group.attr + "]");
 
-        if (inputs.size() > 0) {
+        if (inputs.length > 0) {
             Bennu.group.setupModal();
         }
 
