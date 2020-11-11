b3d13f776ceb32ec0c9f6bbb74f739333406e785
Luis Cruz <luis.cruz@tecnico.pt>
2020-11-11 13:36:56 +0000

    Work in progress.

--- bennu-spring/src/main/java/org/fenixedu/bennu/spring/BaseController.java
+++ bennu-spring/src/main/java/org/fenixedu/bennu/spring/BaseController.java
@@ -9,6 +9,7 @@ import org.fenixedu.bennu.core.security.Authenticate;
 import org.fenixedu.bennu.core.util.CoreConfiguration;
 import org.fenixedu.commons.i18n.I18N;
 import org.fenixedu.commons.stream.StreamUtils;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -17,18 +18,46 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.nio.charset.StandardCharsets;
+import java.util.function.Consumer;
 import java.util.stream.Stream;
 
 public class BaseController {
 
-    public static ResponseEntity<?> ok(final JsonElement body) {
-        final ResponseEntity.BodyBuilder builder = ResponseEntity.ok();
+    public static ResponseEntity<?> respond(final HttpStatus status, final JsonElement body) {
+        final ResponseEntity.BodyBuilder builder = ResponseEntity.status(status);
         builder.contentType(new MediaType("application", "json", StandardCharsets.UTF_8));
         return builder.body(body.toString());
     }
 
+    public static ResponseEntity<?> ok(final JsonElement body) {
+        return respond(HttpStatus.OK, body);
+    }
+
     public static ResponseEntity<?> respond(final Stream<JsonObject> stream) {
         return ok(stream.collect(StreamUtils.toJsonArray()));
+    }
+
+    public static ResponseEntity<?> respond(final HttpStatus status, final Consumer<JsonObject> consumer,
+                                            final String bundle, final String messageKey, final String... args) {
+        final JsonObject response = new JsonObject();
+        response.addProperty("message", BundleUtil.getString(bundle, I18N.getLocale(), messageKey, args));
+        final JsonObject data = new JsonObject();
+        if (consumer != null) {
+            consumer.accept(data);
+        }
+        response.add("data", data);
+        return respond(status, response);
+    }
+
+    public static ResponseEntity<?> redirect(final String relativePath) {
+        final HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", CoreConfiguration.getConfiguration().applicationUrl().concat(relativePath));
+        return new ResponseEntity<byte[]>(null, headers, HttpStatus.TEMPORARY_REDIRECT);
+    }
+
+    public static ResponseEntity<?> respond(final Consumer<JsonObject> consumer, final String bundle,
+                                            final String messageKey, final String... args) {
+        return respond(HttpStatus.OK, consumer, bundle, messageKey);
     }
 
     public static ResponseEntity<?> unauthorized() {
@@ -41,25 +70,38 @@ public class BaseController {
         return ResponseEntity.notFound().build();
     }
 
+    public static ResponseEntity<?> respondNotFound(final String bundle, final String messageKey, final String... args) {
+        final String message = BundleUtil.getString(bundle, I18N.getLocale(), messageKey, args);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(message);
+    }
+
     public static ResponseEntity<?> respondPreconditionFailed(final String errorMessage) {
         return ResponseEntity.status(HttpStatus.PRECONDITION_FAILED).body(errorMessage);
     }
 
-    static ResponseEntity<?> respondLiteralBadRequest(final String message) {
+    public static ResponseEntity<?> respondLiteralBadRequest(final String message) {
         return Strings.isNullOrEmpty(message) ? ResponseEntity.badRequest().build()
                 : ResponseEntity.badRequest().body(message);
     }
 
-    public static ResponseEntity<?> respondBadRequest(final String messageKey) {
+    public static ResponseEntity<?> respondBadRequest(final String bundle, final String messageKey) {
         if (Strings.isNullOrEmpty(messageKey)) {
             return respondLiteralBadRequest(null);
         }
-        final String message = BundleUtil.getString("resources.AdmissionsResources", I18N.getLocale(), messageKey);
+        final String message = BundleUtil.getString(bundle, I18N.getLocale(), messageKey);
+        return respondLiteralBadRequest(message);
+    }
+
+    public static ResponseEntity<?> respondBadRequest(final String bundle, final String messageKey, final String... args) {
+        if (Strings.isNullOrEmpty(messageKey)) {
+            return respondLiteralBadRequest(null);
+        }
+        final String message = BundleUtil.getString(bundle, I18N.getLocale(), messageKey, args);
         return respondLiteralBadRequest(message);
     }
 
     public static ResponseEntity<?> respondBadRequest() {
-        return respondBadRequest(null);
+        return respondBadRequest(null, null);
     }
 
     public static ResponseEntity<?> respondConflict() {
