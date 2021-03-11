ed5b8ac02dee5505e13627e8bea32c90591eaea6
tiagoppinho <tiago.m.pinho@tecnico.ulisboa.pt>
2021-03-11 14:46:01 +0000

    Support responding pagination results using spring BaseController. BNN-421 #resolve

--- bennu-spring/src/main/java/org/fenixedu/bennu/spring/BaseController.java
+++ bennu-spring/src/main/java/org/fenixedu/bennu/spring/BaseController.java
@@ -1,10 +1,12 @@
 package org.fenixedu.bennu.spring;
 
 import com.google.common.base.Strings;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import org.fenixedu.bennu.core.domain.User;
 import org.fenixedu.bennu.core.i18n.BundleUtil;
+import org.fenixedu.bennu.core.json.JsonUtils;
 import org.fenixedu.bennu.core.security.Authenticate;
 import org.fenixedu.bennu.core.util.CoreConfiguration;
 import org.fenixedu.commons.i18n.I18N;
@@ -18,7 +20,10 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.function.BiFunction;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.stream.Stream;
 
 public class BaseController {
@@ -47,6 +52,81 @@ public class BaseController {
         }
         response.add("data", data);
         return respond(status, response);
+    }
+
+    /**
+     * Responds the pagination results accordingly with the default page mapper.
+     *
+     * <p>Check {@link BaseController#respondPagination(Long, Long, List, Function, BiFunction)}
+     * for the proper method documentation.</p>
+     *
+     * <p>Also check {@link BaseController#toPaginationPage(int, Stream)} for the default
+     * page mapper.</p>
+     *
+     * @see BaseController#respondPagination(Long, Long, List, Function, BiFunction).
+     * @param skip {@link Long} value to use as stream skip.
+     * @param limit {@link Long} value to use as stream limit.
+     * @param results {@link List} containing all results to be used, this value cannot be {@code null}.
+     * @param resultMapper {@link Function} to be used as the result mapper, this value cannot be {@code null}.
+     * @return {@link ResponseEntity} with an {@link HttpStatus} of 200 (ok) and the correspondent body.
+     */
+    public static <T> ResponseEntity<?> respondPagination(final Long skip, final Long limit, final List<T> results,
+                                                          final Function<T, JsonObject> resultMapper) {
+        return respondPagination(skip, limit, results, resultMapper, BaseController::toPaginationPage);
+    }
+
+    /**
+     * Responds a {@link ResponseEntity} with an {@link HttpStatus} of 200 (ok).
+     *
+     * <p>If either skip or limit are {@code null}, the response body will contain a
+     * {@link JsonArray} with the mapped results.</p>
+     *
+     * <p>If both skip and limit values are passed, the response body will contain a
+     * {@link JsonObject} with a {@link Integer} value of total items {@code "totalItems"}
+     * and a {@link JsonArray} containing all the mapped results with the
+     * skip and limit values applied {@code "items"}.</p>
+     *
+     * @param skip {@link Long} value to use as stream skip.
+     * @param limit {@link Long} value to use as stream limit.
+     * @param results {@link List} containing all results to be used, this value cannot be {@code null}.
+     * @param resultMapper {@link Function} to be used as the result mapper, this value cannot be {@code null}.
+     * @param pageMapper {@link BiFunction} to be used as the page mapper, this value cannot be {@code null}.
+     * @return {@link ResponseEntity} with an {@link HttpStatus} of 200 (ok) and the correspondent body.
+     */
+    public static <T> ResponseEntity<?> respondPagination(final Long skip, final Long limit, final List<T> results,
+                                                          final Function<T, JsonObject> resultMapper,
+                                                          final BiFunction<Integer, Stream<JsonObject>, JsonObject> pageMapper) {
+        final int totalItems = results.size();
+        final Stream<JsonObject> result = results.stream()
+                .skip(skip == null ? 0L : skip)
+                .limit(limit == null ? Long.MAX_VALUE : limit)
+                .map(resultMapper);
+
+        if (skip != null && limit != null) {
+            return ok(pageMapper.apply(totalItems, result));
+        }
+        return respond(result);
+    }
+
+    /**
+     * This method is the default pagination page mapper.
+     *
+     * <p>Returns a {@link JsonObject} containing the {@code "totalItems"} param value
+     * and a {@link JsonArray} {@code "items"} value of multiple {@link JsonObject}
+     * containing the collected result.</p>
+     *
+     * <p>It is used in the {@link BaseController#respondPagination(Long, Long, List, Function)}
+     * method as default page mapper.</p>
+     *
+     * @param totalItems int value of total items.
+     * @param result {@link Stream} of {@link JsonObject} containing the result to be collected.
+     * @return {@link JsonObject} with the corresponding properties.
+     */
+    public static JsonObject toPaginationPage(final int totalItems, final Stream<JsonObject> result) {
+        return JsonUtils.toJson(data -> {
+            data.addProperty("totalItems", totalItems);
+            data.add("items", result.collect(StreamUtils.toJsonArray()));
+        });
     }
 
     public static ResponseEntity<?> redirect(final String relativePath) {
