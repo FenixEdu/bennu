15202293d49b2a9871f55ad68c910a97ff211962
Guilherme Marcondes <guilherme.marcondes@tecnico.ulisboa.pt>
2024-01-12 18:06:16 +0000

    Added wrapper to set slots, roles and relations of a domain object

--- bennu-admin/src/main/java/org/fenixedu/bennuAdmin/util/DynamicFormAdapter.java
+++ bennu-admin/src/main/java/org/fenixedu/bennuAdmin/util/DynamicFormAdapter.java
@@ -1,4 +1,12 @@
 package org.fenixedu.bennuAdmin.util;
 
+import org.fenixedu.bennu.core.json.JsonUtils;
+import pt.ist.fenixframework.DomainObject;
+
 public class DynamicFormAdapter {
+  private DomainObject domainObject;
+
+  public DynamicFormAdapter(DomainObject domainObject) {
+    domainObject = domainObject;
+  }
 }
